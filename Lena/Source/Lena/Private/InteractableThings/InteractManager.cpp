57ba2a9f03effe23ab18f22e39d42edcc9dea1ae
ChangJin-Lee <ckdwls525@gmail.com>
2024-07-12 17:27:36 +0900

    feat: InteractManager DataTable Mapping

--- Lena/Source/Lena/Private/InteractableThings/InteractManager.cpp
+++ Lena/Source/Lena/Private/InteractableThings/InteractManager.cpp
@@ -1,39 +1,124 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "InteractableThings/AInteractManager.h"
+#include "InteractableThings/InteractManager.h"
 #include "EngineUtils.h"
+#include "InteractableThings/Door/DoorActor.h"
+#include "InteractableThings/Lock/LockActor.h"
+#include "Engine/World.h"
+#include "Interface/InteractActionInterface.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
-AAInteractManager::AAInteractManager()
+AInteractManager::AInteractManager()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 }
 
 // Called when the game starts or when spawned
-void AAInteractManager::BeginPlay()
+void AInteractManager::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// 액터 매핑 초기화
-	for(TActorIterator<AActor> It(GetWorld()); It; ++It) // #include "EngineUtils.h" 필요
-	{
-		AActor* Actor = *It;
-		if(Actor)
-		{
-			ActorMap.Add(Actor->GetName(), Actor);
-		}
-	}
+	// 특정 레벨에 대한 DataTable을 로드
+	FString LevelName = GetWorld()->GetMapName();
+	LoadDataTableForLevel(LevelName);
 
+
+	// Set a timer to call SetupLocksAndDoors on the next tick
+	GetWorld()->GetTimerManager().SetTimerForNextTick(this, &AInteractManager::SetupLockAndDoor);
 	// 매핑설정
-	SetupMappings();
+	// SetupLockAndDoor();
 }
 
 // Called every frame
-void AAInteractManager::Tick(float DeltaTime)
+void AInteractManager::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
 
-AAInteractManager
+void AInteractManager::LoadDataTableForLevel(const FString& LevelName)
+{
+	FString DataTablePath = FString::Printf(TEXT("DataTable'/Game/Data/DataTables/%s_InteractConditionTable.%s_InteractConditionTable'"), *LevelName, *LevelName);
+	static ConstructorHelpers::FObjectFinder<UDataTable> LockDoorMappingData(*DataTablePath);
+
+	if (LockDoorMappingData.Succeeded())
+	{
+		LockDoorMappingTable = LockDoorMappingData.Object;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to load DataTable for level: %s"), *LevelName);
+	}
+}
+
+
+void AInteractManager::SetupLockAndDoor()
+{
+	// 액터 매핑 초기화
+	for(TActorIterator<AActor> It(GetWorld()); It; ++It) // #include "EngineUtils.h" 필요
+	{
+		AActor* Actor = *It;
+		if(Actor->IsA<ALockActor>())
+		{
+			LockMap.Add(Actor->GetActorLabel(), Actor);
+		}
+		else if (Actor->IsA<ADoorActor>())
+		{
+			DoorMap.Add(Actor->GetActorLabel(), Actor);
+		}
+	}
+	
+	const FString ContextString(TEXT("LockDoorMappingTableContext"));
+	TArray<FActorEntry*> AllRows;
+	LockDoorMappingTable->GetAllRows(ContextString, AllRows);
+
+	for(FActorEntry* Row : AllRows)
+	{
+		if(Row)
+		{
+			AActor** FoundActor = DoorMap.Find(Row->ActorID);
+			if(FoundActor)
+			{
+				for(const FConditionEntry& Condition : Row->RequiredConditions)
+				{
+					SetupConditionWithActor(*FoundActor, Condition);
+				}
+			}
+		}
+	}
+}
+
+void AInteractManager::SetupConditionWithActor(AActor* Actor, const FConditionEntry& ConditionEntry)
+{
+	UE_LOG(LogTemp, Warning, TEXT("SetupConditionWithActor start"));
+	if(ConditionEntry.ConditionType == "Lock")
+	{
+		AActor** FoundLock = LockMap.Find(ConditionEntry.ConditionID);
+		if(FoundLock)
+		{
+			ALockActor* LockActor = Cast<ALockActor>(*FoundLock);
+			ADoorActor* DoorActor = Cast<ADoorActor>(Actor);
+			if(LockActor)
+			{
+				// 특정 자물쇠와 문을 연결하는 로직 추가
+				// 예를 들어, 자물쇠가 풀리면 문을 여는 로직
+				LockActor->Password = ConditionEntry.AdditionalData;
+				LockActor->TargetDoor = DoorActor;
+			}
+		}
+	}
+	else if (ConditionEntry.ConditionType == "Item")
+	{
+		
+	}
+	else if (ConditionEntry.ConditionType == "Dialogue")
+	{
+	
+	}
+	else if (ConditionEntry.ConditionType == "Location")
+	{
+		
+	}
+}
